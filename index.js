23d550d76b3db845c3089310878ee977ee2901b9
Salma Jediel <giannasal03@gmail.com>
2022-06-07 09:21:13 +0300

    commit tree 45a174b2ab3e4b8e19ad384f6059971c007d8075

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
+const num1 = (31);
+const num2 = (2);
+let multiply;
+multiply = (num1)*(num2);
 // Write your code here
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = (10);
+const num4 = (6);
+let mod;
+mod = (num3)%(num4);
+
+let max;
+max = Math.max(1,2,3,4,5,6,7,8,20);
